3ce1fcd4301af77192270c44c01da2425f08ff41
Nikita Skorodumov <mr-zerx@yandex.ru>
2017-11-26 00:47:36 +0500

    Доработал кнопку закрытия меню

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,7 @@
 var hamburgerButton = document.getElementById('hamburger-button')
 var hamburgerMenu = document.getElementById('hamburger-menu')
+var hamburgerIcon = document.getElementById('hamburger-picture')
+var exitButton = document.getElementById('lineblock')
 var isOpen = false; 
 
 hamburgerButton.addEventListener ('click', function() {
@@ -7,8 +9,14 @@ hamburgerButton.addEventListener ('click', function() {
     isOpen = isOpen === true ? false : true
     if (isOpen) {
         hamburgerMenu.classList.add('greeting__menu-active')
+        hamburgerIcon.classList.add('hamburger-menu__picture-active')
+        exitButton.classList.add('hamburger-menu-lineblock-active')
+        
+        
     }
     if (!isOpen) {
         hamburgerMenu.classList.remove('greeting__menu-active')
+        hamburgerIcon.classList.remove('hamburger-menu__picture-active')
+        exitButton.classList.remove('hamburger-menu-lineblock-active')
     }
 })
